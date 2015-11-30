5190e0056f5c2ff4247998a8c67eabe3b5db3ab3
KovalM <m.koval@mail.by>
2015-11-30 03:12:42 +0300

    Complete

--- src/model/ImageModel.java
+++ src/model/ImageModel.java
@@ -9,7 +9,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class ImageModel {
     private int imageWidth;
@@ -17,7 +16,7 @@ public class ImageModel {
     private int n;
     private int m;
     private int p;
-    private double e;
+    private int e;
     private BufferedImage inputImage;
     private List<RectangleModel> rectangleModelList;
     private Matrix W;
@@ -29,16 +28,17 @@ public class ImageModel {
             inputImage = ImageIO.read(f);
             imageWidth = inputImage.getWidth();
             imageHight = inputImage.getHeight();
-            Scanner in = new Scanner(System.in);
-            System.out.println("Enter hight of rectangle(n):");
-            n = in.nextInt();
-            System.out.println("Enter width of rectangle(m):");
-            m = in.nextInt();
-            System.out.println("Enter number of neuron for second layer(p):");
-            p = in.nextInt();
+//            Scanner in = new Scanner(System.in);
+//            System.out.println("Enter hight of rectangle(n):");
+//            n = in.nextInt();
+//            System.out.println("Enter width of rectangle(m):");
+//            m = in.nextInt();
+//            System.out.println("Enter number of neuron for second layer(p):");
+//            p = in.nextInt();
 //            System.out.println("Enter error degree(e):");
 //            e = in.nextInt();
-            in.close();
+//            in.close();
+            n = 5; m = 5; p = 4; e = 8000;
             createRectangleList();
             createWeightMatrix();
             startWork();
@@ -47,24 +47,83 @@ public class ImageModel {
         }
     }
 
+    public BufferedImage createOutputImage(){
+        BufferedImage answer = new BufferedImage(imageHight, imageWidth, BufferedImage.TYPE_INT_RGB);
+        for (RectangleModel currentRectangle : rectangleModelList){
+            Matrix X = currentRectangle.getX();
+            Matrix Y = X.times(W);
+            Matrix X_ = Y.times(W_);
+            int xx = currentRectangle.getStartX();
+            int yy = currentRectangle.getStartY();
+            int l=0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    int r = (int)convertRGBToOutput(X_.get(0, l++));
+                    int g = (int)convertRGBToOutput(X_.get(0, l++));
+                    int b = (int)convertRGBToOutput(X_.get(0, l++));
+                    Color currentColor = new Color(r, g, b);
+                    if (xx+i<imageHight && yy+j<imageWidth){
+                        answer.setRGB(xx+i, yy+j, currentColor.getRGB());
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+
+    private double convertRGBToOutput(double rgb){
+        double ans = 255*(rgb+1)/2;
+        if (ans<0) ans=0;
+        if (ans>255) ans=255;
+        return ans;
+    }
     private void startWork(){
-        int t=1;
-        double E=10000;
+        int t = 1;
+        double E=1000000;
         while (E>e){
             int l = rectangleModelList.size();
-            double sumY;
+            double alpha;
+            double alpha_;
+            E=0;
             for (int i = 0; i < l; i++) {
                 Matrix X = rectangleModelList.get(i).getX();
                 Matrix Y = X.times(W);
                 Matrix X_ = Y.times(W_);
                 Matrix deltaX = X_.minus(X);
-
+                alpha=1/calculateVectorSum(X);
+                alpha_=1/calculateVectorSum(Y);
+                W = W.minus(X.transpose().times(alpha).times(deltaX).times(W_.transpose()));
+                W_ = W_.minus(Y.transpose().times(alpha_).times(deltaX));
+                normalizeMatrix(W);
+                normalizeMatrix(W_);
+                E += getErrorDegree(X, X_);
             }
-
-
+            System.out.println("Iteration: "+t+"; Error: "+E);
             t++;
         }
     }
+
+    private double getErrorDegree(Matrix X, Matrix X_){
+        double e=0;
+        for (int i = 0; i < X.getColumnDimension(); i++) {
+            e += Math.pow(X_.get(0, i) - X.get(0, i), 2);
+        }
+        return e;
+    }
+    private void normalizeMatrix(Matrix matrix){
+        for (int i = 0; i < matrix.getRowDimension(); i++) {
+            double sum = 0;
+            for (int j = 0; j < matrix.getColumnDimension(); j++) {
+                sum +=Math.pow(matrix.get(i, j), 2);
+            }
+            sum = Math.sqrt(sum);
+
+            for (int j = 0; j < matrix.getColumnDimension(); j++) {
+                matrix.set(i, j, matrix.get(i, j) / sum);
+            }
+        }
+    }
+
     private void createWeightMatrix(){
         double bufferW[][] = new double[n*m*3][p];
         for (int i = 0; i < n*m*3; i++) {
@@ -129,5 +188,14 @@ public class ImageModel {
             }
             System.out.println();
         }
+    }
+
+    public double calculateVectorSum(Matrix currentMatrix){
+        if (currentMatrix.getRowDimension()>1) return 0;
+        double sum=0;
+        for (int i = 0; i < currentMatrix.getColumnDimension(); i++) {
+            sum+=Math.pow(currentMatrix.get(0, i), 2);
+        }
+        return sum;
     }
 }
--- src/process/ImageProcess.java
+++ src/process/ImageProcess.java
@@ -7,21 +7,14 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class ImageProcess {
-    private BufferedImage inputImage;
     private BufferedImage outputImage;
     private ImageModel imageModel;
 
     public ImageProcess(String imageName){
         try {
             imageModel = new ImageModel(imageName);
-            /*int width = inputImage.getWidth();
-            int height = inputImage.getHeight();
-            outputImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
-
-            Graphics g = outputImage.getGraphics();
-            g.drawImage(inputImage, 0, 0, null);
-            g.dispose();
-            ImageIO.write(outputImage, "jpg", new File("2.jpg"));*/
+            outputImage = imageModel.createOutputImage();
+            ImageIO.write(outputImage, "jpg", new File("2.jpg"));
         } catch (Exception e) {
             e.printStackTrace();
         }
